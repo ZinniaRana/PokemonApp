18c2255743329643bb9b98501992c03db6f220d6
ZinniaRana <zinnia.rana.22@gmail.com>
2021-12-12 23:55:56 -0400

    Removed unused files

--- src/API/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-export async function fetchPokemons(offset, limit){
-    const response = await axios
-                            .get(`${this.apiUrl}?offset=${offset}&limit=${limit}`);
-    return(response.data)
-}
